ba33a99ead9ff5163abc6465e8741709abe1b0e5
Linpeng Zhang <linpeng.zhang@lacework.net>
2023-02-02 09:58:16 +0000

    address some reviewer comments

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+SCA_VERSION=0.0.13
+SAST_VERSION=0.0.27
+
 echo "::group::Installing Lacework CLI components"
-lacework --noninteractive -a "${LW_ACCOUNT_NAME}" -k "${LW_API_KEY}" -s "${LW_API_SECRET}" component install sca --version 0.0.13
-lacework --noninteractive -a "${LW_ACCOUNT_NAME}" -k "${LW_API_KEY}" -s "${LW_API_SECRET}" component install sast --version 0.0.25
+lacework --noninteractive -a "${LW_ACCOUNT_NAME}" -k "${LW_API_KEY}" -s "${LW_API_SECRET}" component install sca --version "${SCA_VERSION}"
+lacework --noninteractive -a "${LW_ACCOUNT_NAME}" -k "${LW_API_KEY}" -s "${LW_API_SECRET}" component install sast --version "${SAST_VERSION}"
 echo "::endgroup::"
 echo "::group::Printing Lacework CLI information"
 lacework --noninteractive -a "${LW_ACCOUNT_NAME}" -k "${LW_API_KEY}" -s "${LW_API_SECRET}" version

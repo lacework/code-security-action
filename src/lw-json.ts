b951c71a8086e1585dc307bfdb7adaff8092af51
Linpeng Zhang <linpeng.zhang@lacework.net>
2024-02-01 15:54:07 +0000

    support ariadne recommendations

--- src/lw-json.ts
+++ src/lw-json.ts
@@ -2,18 +2,41 @@ export interface LWJSON {
   FixSuggestions?: FixSuggestion[]
 }
 
-// Fix suggestion
+export interface VersionEdge {
+  Version: string
+  Severity?: string
+  CVE?: string
+}
+
+export interface VersionNode {
+  Version: string
+  Edges?: VersionEdge[]
+}
+
+export interface VersionGraphInfo {
+  NodeCnt?: number
+  EdgeCnt?: number
+  ExtraAthenaCalls?: number
+  VersionGraph?: VersionNode[]
+}
+
+export interface Recommendation {
+  FixVersion: FixVersion
+  Kind?: string
+}
+
+export interface Info {
+  Recommendations?: Recommendation[]
+  VersionGraphInfo?: VersionGraphInfo
+}
+
+export interface FixSuggestion {
+  Id: string
+  Info: Info
+  AffectedArtifactId: string
+}
 
 export interface FixVersion {
   Type?: string
   Version?: string
-}
-
-export interface Info {
-  FixVersion?: FixVersion
-  Diffs?: string[][]
-}
-export interface FixSuggestion {
-  FixId: string
-  Info: Info
 }
